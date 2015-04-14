b5c4937d465e8249743beca6d6f5960cc559f8fc
Tambet Matiisen <tambet.matiisen@gmail.com>
2015-04-14 11:28:22 +0300

    Tiny compatibility change for Matlab.

--- 2015/Practices/08 - Artificial Neural Networks/code/confusionmat.m
+++ 2015/Practices/08 - Artificial Neural Networks/code/confusionmat.m
@@ -1,7 +1,7 @@
 % http://wiki.octave.org/Talk:Statistics_package
  function [ret] = confusionmat(v1, v2)
     values = union(unique(v1), unique(v2));
-    ret = zeros(size(values), size(values));
+    ret = zeros(size(values, 1), size(values, 1));
     for i = 1:size(v1)
        i1 = find(values == v1(i));
        i2 = find(values == v2(i));
