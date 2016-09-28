a350e28834466079b7a892d6dcbd77e4246a309b
Ardi <arditampuu@gmail.com>
2016-09-28 16:58:41 +0300

    Practice session 3: machine learning

---
+++ 2016/Practices/03-Machine Learning/code/helpers/confusionmat.m
@@ -0,0 +1,10 @@
+% http://wiki.octave.org/Talk:Statistics_package
+ function [ret, values] = confusionmat(v1, v2)
+    values = union(unique(v1), unique(v2));
+    ret = zeros(size(values, 1), size(values, 1));
+    for i = 1:size(v1)
+       i1 = find(values == v1(i));
+       i2 = find(values == v2(i));
+       ret(i1, i2) = ret(i1, i2) + 1;
+    end
+ end
---
+++ 2016/Practices/03-Machine Learning/code/helpers/splitdata.m
@@ -0,0 +1,21 @@
+%
+% Split dataset into training, validation and test subsets
+%
+function [train_features, train_labels, val_features, val_labels] = splitdata(features, labels, proportion)
+
+  % pick random indices for the training subset
+  shuffled_idx = randperm(length(labels));
+  train_idx = shuffled_idx(1:floor(length(labels) * proportion));
+
+  % split the rest between the validation and the test subset
+  val_idx = shuffled_idx(floor(length(labels) * proportion):length(labels));
+
+  % split the feature matrix
+  train_features = features(train_idx, :);
+  val_features = features(val_idx, :);
+
+  % split the class label vector
+  train_labels = labels(train_idx);
+  val_labels = labels(val_idx);
+end
+
